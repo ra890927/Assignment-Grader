3550f0f38cc89c1198955a31594652664c7cc10c
ra890927 <happy.890927410@gmail.com>
2023-11-10 23:56:51 +0800

    feat: using visual studio instead

--- module/compiler.py
+++ module/compiler.py
@@ -1,23 +1,20 @@
 from typing import List
 from pathlib import Path
 from subprocess import call
-from shutil import copy, copytree
+from configparser import ConfigParser
 
-from .utils import grep, insert, replace_all, change_file_encoding
+from .utils import grep, change_file_encoding
 
 
 class Compiler:
-    def __init__(self, problem_num: int, exec_path: Path) -> None:
+    def __init__(self, code_pat: str, problem_num: int, exec_path: Path) -> None:
+        self.code_pat = code_pat
         self.exec_path = exec_path
         self.problem_num = problem_num
-        self.makefile_path = self.__gen_makefile()
-        self.additional_path = Path('./additional')
 
-    def __del__(self) -> None:
-        try:
-            self.makefile_path.unlink()
-        except:
-            pass
+        config = ConfigParser()
+        config.read('./docs/config.ini')
+        self.env_path = Path(config['compiler']['VISUAL_STUDIO_CMD'])
 
     def __call__(self, std_id: str) -> list[bool]:
         return [
@@ -28,40 +25,16 @@ class Compiler:
             )
         ]
 
-    def __gen_makefile(self) -> Path:
-        makefile_path = Path('./Makefile')
-        with open(makefile_path, 'w+', encoding='utf-8') as f:
-            f.write('all:\n')
-            for pid in range(1, self.problem_num + 1):
-                f.write(
-                    '\tg++ -O2 -w *_hw_{:02d}.cpp -lm -o std_exec_{:02d}\n'
-                    .format(pid, pid)
-                )
-        return makefile_path
-
     def __preprocessing(self, std_id: str) -> List[bool]:
         legal = [True for _ in range(self.problem_num)]
         for pid in range(1, self.problem_num + 1):
-            file_path = self.exec_path.joinpath(
-                '{}_hw_{:02d}.cpp'.format(std_id, pid)
-            )
+            file_path = self.exec_path.joinpath(self.code_pat.format(std_id, pid))
             try:
                 change_file_encoding(file_path)
-                self.__replace(file_path)
                 self.__check_legal(file_path)
             except:
                 legal[pid - 1] = False
         return legal
-
-    def __replace(self, file_path: Path) -> None:
-        insert('#include <stdbool.h>\n', file_path)
-        insert('#include "vs_utils.h"\n', file_path)
-        # remove system(pause)
-        replace_all(
-            r'system\(\ *pause\ *\)',
-            '',
-            file_path
-        )
 
     def __check_legal(self, file_path) -> bool:
         return all([
@@ -72,15 +45,18 @@ class Compiler:
         ])
 
     def __compile(self) -> bool:
-        copy(self.makefile_path, self.exec_path)
-        copytree(self.additional_path, self.exec_path, dirs_exist_ok=True)
-
-        call(f'cd {self.exec_path}; make', shell=True)
+        # compile
+        for pid in range(1, self.problem_num + 1):
+            code_name = self.code_pat.format('*', pid)
+            call(
+                f'cl /W4 /sdl {self.exec_path}\\{code_name} /Fe{self.exec_path}\\std_exec_{pid:02d}.exe',
+                shell=True, executable=self.env_path
+            )
 
         legal = [True for _ in range(self.problem_num)]
         for pid in range(1, self.problem_num + 1):
             try:
-                assert self.exec_path.joinpath(f'std_exec_{pid:02d}').exists()
+                assert self.exec_path.joinpath(f'std_exec_{pid:02d}.exe').exists()
             except:
                 legal[pid - 1] = False
         return legal
